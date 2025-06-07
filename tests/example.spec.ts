9efb717fe16b5676265e652677f278a53b10b1d5
Maks Karakusha <maks.karakusha@gmail.com>
2025-06-07 19:39:38 +0300

    Modify API response

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,19 +1,43 @@
 import { test, expect } from '@playwright/test';
 import { PageManager } from '../page-objects/pageManager';
+import tags from '../test-data/tags.json'
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('/');
+test.describe('Test Playwright web site', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+
+  test('has title', async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
+
+  test('get started link', async ({ page }) => {
+    const pm = new PageManager(page);
+    await pm.navigateTo().getStartedPage();
+
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });
 });
 
-test('has title', async ({ page }) => {
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test.describe('Work with API', () => {
+  test.beforeEach(async ({ page }) => {
+    const pm = new PageManager(page);
 
-test('get started link', async ({ page }) => {
-  const pm = new PageManager(page);
-  await pm.navigateTo().getStartedPage();
+    await page.route('*/**/api/tags', async route => {
+      await route.fulfill({
+        body: JSON.stringify(tags)
+      });
+    });
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    pm.navigateTo().conduitPage();
+  });
+
+  test('mock API', async ({ page }) => {
+    const pm = new PageManager(page);
+
+    await page.waitForResponse('*/**/api/tags');
+    await expect(pm.lookAtLandingPage().sideBar).toContainText('Modified tag 1 Modified tag 2');
+  });
 });
