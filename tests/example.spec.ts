642968638e3452aefc88620f62e29d2010eab4b5
Maks Karakusha <maks.karakusha@gmail.com>
2025-06-07 20:53:10 +0300

    Move fixture to the test-data folder

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { PageManager } from '../page-objects/pageManager';
 import tags from '../test-data/tags.json'
+import articleStructure from '../test-data/articleStructure'
+
 
 test.describe('Test Playwright web site', () => {
   test.beforeEach(async ({ page }) => {
@@ -44,40 +46,22 @@ test.describe('Work with API', () => {
   });
 
   test('check the response structure', async ({ page }) => {
-    // const response = await page.waitForResponse('*/**/api/articles*');
-    // expect(response.status()).toBe(200);
+    const responseBody = await page.waitForResponse('*/**/api/articles*').then(response => {
+      expect(response.status()).toBe(200);
+      return response.json();
+    });
 
-  const responseBody = await page.waitForResponse('*/**/api/articles*').then(response => response.json());
+    const expectedArticleStructure = {
+      articles: expect.any(Array),
+      articlesCount: expect.any(Number)
+    };
 
-  // Define the expected structure for an article object
-  const expectedArticleStructure = {
-    articles: expect.any(Array),
-    articlesCount: expect.any(Number)
-  };
+    expect(responseBody).toEqual(expectedArticleStructure);
 
-  expect(responseBody).toEqual(expectedArticleStructure);
-
-  // Optionally, you can also validate the structure of the first article
-  if (responseBody.articles.length > 0) {
-    const firstArticle = responseBody.articles[0];
-      const expectedArticleObjectStructure = {
-        slug: expect.any(String),
-        title: expect.any(String),
-        description: expect.any(String),
-        body: expect.any(String),
-        tagList: expect.any(Array),
-        createdAt: expect.any(String),
-        updatedAt: expect.any(String),
-        favorited: expect.any(Boolean),
-        favoritesCount: expect.any(Number),
-        author: {
-          username: expect.any(String),
-          bio: null,
-          image: expect.any(String),
-          following: expect.any(Boolean),
-        },
-      };
-      expect(firstArticle).toEqual(expectedArticleObjectStructure);
-  };
+    if (responseBody.articles.length > 0) {
+      const firstArticle = responseBody.articles[0];
+        const expectedArticleObjectStructure = articleStructure
+        expect(firstArticle).toEqual(expectedArticleObjectStructure);
+    };
   });
 });
